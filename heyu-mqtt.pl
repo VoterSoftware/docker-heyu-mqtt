817a416d70731b1f475ee5a30d86d14c1226dabc
VoterSoftware <54810951+VoterSoftware@users.noreply.github.com>
2026-01-02 19:03:09 -0500

    Enhance heyu-mqtt.pl with CM17A support
    Added support for CM17A Firecracker command translation.

--- heyu-mqtt.pl
+++ heyu-mqtt.pl
@@ -5,38 +5,57 @@ use AnyEvent::MQTT;
 use AnyEvent::Run;
 
 my $config = {
-    mqtt_host => $ENV{MQTT_HOST} || 'localhost',
-    mqtt_port => $ENV{MQTT_PORT} || '1883',
-    mqtt_user => $ENV{MQTT_USER},
-    mqtt_password => $ENV{MQTT_PASSWORD},
-    mqtt_prefix => $ENV{MQTT_PREFIX} || 'home/x10',
+    mqtt_host      => $ENV{MQTT_HOST} || 'localhost',
+    mqtt_port      => $ENV{MQTT_PORT} || '1883',
+    mqtt_user      => $ENV{MQTT_USER},
+    mqtt_password  => $ENV{MQTT_PASSWORD},
+    mqtt_prefix    => $ENV{MQTT_PREFIX} || 'home/x10',
     mqtt_retain_re => qr/$ENV{MQTT_RETAIN_RE}/i || qr//, # retain everything
-    heyu_cmd => $ENV{HEYU_CMD} || 'heyu',
+    heyu_cmd       => $ENV{HEYU_CMD} || 'heyu',
+
+    # NEW: if true, translate on/off -> fon/foff for CM17A Firecracker
+    use_cm17       => ($ENV{USE_CM17} && $ENV{USE_CM17} =~ /^(1|true|yes|on)$/i) ? 1 : 0,
 };
 
 my $mqtt = AnyEvent::MQTT->new(
-    host => $config->{mqtt_host},
-    port => $config->{mqtt_port},
+    host      => $config->{mqtt_host},
+    port      => $config->{mqtt_port},
     user_name => $config->{mqtt_user},
-    password => $config->{mqtt_password},
+    password  => $config->{mqtt_password},
 );
 
 sub receive_mqtt_set {
     my ($topic, $message) = @_;
     $topic =~ m{\Q$config->{mqtt_prefix}\E/(.*)/set};
     my $device = $1;
-    if ($device eq "raw") { # send 'raw' message, for example "xpreset o3 32" 
-        AE::log info => "X10 sending heuy commmand $message";
-        system($config->{heyu_cmd}, lc $message);
-    } elsif ($message =~ m{^on$|^off$}i) {
-        AE::log info => "X10 switching device $device $message";
-        system($config->{heyu_cmd}, lc $message, $device);
-    } 
+
+    my $msg = lc($message // '');
+
+    if ($device eq "raw") { # send 'raw' message, for example "xpreset o3 32"
+        AE::log info => "X10 sending heyu command $message";
+
+        # IMPORTANT: split multi-word commands into args for system()
+        my @args = split /\s+/, $msg;
+        system($config->{heyu_cmd}, @args);
+
+    } elsif ($msg =~ m{^(on|off)$}) {
+        # Translate on/off to fon/foff when using CM17A Firecracker
+        my $cmd = $msg;
+        if ($config->{use_cm17}) {
+            $cmd = ($cmd eq 'on') ? 'fon' : 'foff';
+        }
+
+        AE::log info => "X10 switching device $device $cmd (from $msg)";
+        system($config->{heyu_cmd}, $cmd, $device);
+    }
 }
 
 sub send_mqtt_status {
     my ($device, $status, $dimlevel) = @_;
-    $mqtt->publish(topic => "$config->{mqtt_prefix}/$device", message => sprintf('"%s"', $status ? 'on' : 'off'));
+    $mqtt->publish(
+        topic   => "$config->{mqtt_prefix}/$device",
+        message => sprintf('"%s"', $status ? 'on' : 'off')
+    );
 }
 
 my $addr_queue = {};
@@ -56,9 +75,9 @@ sub process_heyu_line {
             }
             delete $addr_queue->{$house};
         }
-    } elsif ($line =~ m{  \S+ func\s+(\w+) : hu ([A-Z])(\d+)  level (\d+)}) {                                                                                                                         
-	my ($cmd, $house, $unit, $level) = ($1, $2, $3, $4);                                                                                                                                               
-	process_heyu_cmd(lc $cmd, "$house$unit", $level);                                                                                                                              
+    } elsif ($line =~ m{  \S+ func\s+(\w+) : hu ([A-Z])(\d+)  level (\d+)}) {
+        my ($cmd, $house, $unit, $level) = ($1, $2, $3, $4);
+        process_heyu_cmd(lc $cmd, "$house$unit", $level);
     }
 }
 
